eb64918c39a11fd31d46e71f9d92bb02b911a2d3
uniqueboyy02-maker <uniqueboyy02@gmail.com>
2025-09-16 15:50:48 +0530

    Create abc.php

---
+++ abc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+
+function myTest(){
+    $x = 5;
+    echo"<p>Variable x inside function is:,$x</p>";
+}
+myTest();
+
+echo"<p>Variable x outside function is:$x</p>";
+?>
+ 
+</body>
+</html>
